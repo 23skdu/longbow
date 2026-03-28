1cdd79cd6bd2a6517babef547f0e9689717f7ca6
23skdu <23skdu@users.noreply.github.com>
2026-03-28 12:56:45 -0700

    fix: rename gpu bridge functions to avoid collisions and fix build

--- internal/gpu/faiss_gpu_cpp.h
+++ internal/gpu/faiss_gpu_cpp.h
@@ -14,35 +14,35 @@ typedef void* FaissGpuIndexIVFPtr;
 typedef void* FaissGpuIndexIVFPQPtr;
 
 // Error handling
-int faiss_get_last_error_code();
-const char* faiss_get_last_error_msg();
+int lb_faiss_get_last_error_code();
+const char* lb_faiss_get_last_error_msg();
 
 // GPU Resources
-FaissGpuResourcesPtr faiss_gpu_resources_new(int device);
-void faiss_gpu_resources_free(FaissGpuResourcesPtr ptr);
+FaissGpuResourcesPtr lb_faiss_gpu_resources_new(int device);
+void lb_faiss_gpu_resources_free(FaissGpuResourcesPtr ptr);
 
 // Flat L2 Index
-FaissGpuIndexFlatPtr faiss_gpu_index_flat_l2_new(FaissGpuResourcesPtr res, int dim);
-void faiss_gpu_index_flat_l2_free(FaissGpuIndexFlatPtr ptr);
-int faiss_gpu_index_flat_l2_add(FaissGpuIndexFlatPtr ptr, int64_t n, float* vectors);
-int faiss_gpu_index_flat_l2_search(FaissGpuIndexFlatPtr ptr, int64_t n, float* queries, int k, float* distances, int64_t* labels);
-int faiss_gpu_index_flat_l2_ntotal(FaissGpuIndexFlatPtr ptr);
+FaissGpuIndexFlatPtr lb_faiss_gpu_index_flat_l2_new(FaissGpuResourcesPtr res, int dim);
+void lb_faiss_gpu_index_flat_l2_free(FaissGpuIndexFlatPtr ptr);
+int lb_faiss_gpu_index_flat_l2_add(FaissGpuIndexFlatPtr ptr, int64_t n, float* vectors);
+int lb_faiss_gpu_index_flat_l2_search(FaissGpuIndexFlatPtr ptr, int64_t n, float* queries, int k, float* distances, int64_t* labels);
+int lb_faiss_gpu_index_flat_l2_ntotal(FaissGpuIndexFlatPtr ptr);
 
 // IVF Index
-FaissGpuIndexIVFPtr faiss_gpu_index_ivf_flat_new(FaissGpuResourcesPtr res, int dim, int nlist);
-void faiss_gpu_index_ivf_flat_free(FaissGpuIndexIVFPtr ptr);
-int faiss_gpu_index_ivf_flat_train(FaissGpuIndexIVFPtr ptr, int64_t n, float* vectors);
-int faiss_gpu_index_ivf_flat_add(FaissGpuIndexIVFPtr ptr, int64_t n, float* vectors);
-int faiss_gpu_index_ivf_flat_search(FaissGpuIndexIVFPtr ptr, int64_t n, float* queries, int k, float* distances, int64_t* labels);
-int faiss_gpu_index_ivf_flat_set_nprobe(FaissGpuIndexIVFPtr ptr, int nprobe);
+FaissGpuIndexIVFPtr lb_faiss_gpu_index_ivf_flat_new(FaissGpuResourcesPtr res, int dim, int nlist);
+void lb_faiss_gpu_index_ivf_flat_free(FaissGpuIndexIVFPtr ptr);
+int lb_faiss_gpu_index_ivf_flat_train(FaissGpuIndexIVFPtr ptr, int64_t n, float* vectors);
+int lb_faiss_gpu_index_ivf_flat_add(FaissGpuIndexIVFPtr ptr, int64_t n, float* vectors);
+int lb_faiss_gpu_index_ivf_flat_search(FaissGpuIndexIVFPtr ptr, int64_t n, float* queries, int k, float* distances, int64_t* labels);
+int lb_faiss_gpu_index_ivf_flat_set_nprobe(FaissGpuIndexIVFPtr ptr, int nprobe);
 
 // IVF-PQ Index
-FaissGpuIndexIVFPQPtr faiss_gpu_index_ivf_pq_new(FaissGpuResourcesPtr res, int dim, int nlist, int m, int nbits_per_idx);
-void faiss_gpu_index_ivf_pq_free(FaissGpuIndexIVFPQPtr ptr);
-int faiss_gpu_index_ivf_pq_train(FaissGpuIndexIVFPQPtr ptr, int64_t n, float* vectors);
-int faiss_gpu_index_ivf_pq_add(FaissGpuIndexIVFPQPtr ptr, int64_t n, float* vectors);
-int faiss_gpu_index_ivf_pq_search(FaissGpuIndexIVFPQPtr ptr, int64_t n, float* queries, int k, float* distances, int64_t* labels);
-int faiss_gpu_index_ivf_pq_set_nprobe(FaissGpuIndexIVFPQPtr ptr, int nprobe);
+FaissGpuIndexIVFPQPtr lb_faiss_gpu_index_ivf_pq_new(FaissGpuResourcesPtr res, int dim, int nlist, int m, int nbits_per_idx);
+void lb_faiss_gpu_index_ivf_pq_free(FaissGpuIndexIVFPQPtr ptr);
+int lb_faiss_gpu_index_ivf_pq_train(FaissGpuIndexIVFPQPtr ptr, int64_t n, float* vectors);
+int lb_faiss_gpu_index_ivf_pq_add(FaissGpuIndexIVFPQPtr ptr, int64_t n, float* vectors);
+int lb_faiss_gpu_index_ivf_pq_search(FaissGpuIndexIVFPQPtr ptr, int64_t n, float* queries, int k, float* distances, int64_t* labels);
+int lb_faiss_gpu_index_ivf_pq_set_nprobe(FaissGpuIndexIVFPQPtr ptr, int nprobe);
 
 #ifdef __cplusplus
 }

cf37ba88854eee0c346f6a0a917135c25a33cf6e
23skdu <23skdu@users.noreply.github.com>
2026-04-27 20:24:22 -0700

    Fix sq8 for AVX2-only build, clean up distance_avx512.s

--- internal/simd/distance_avx512.s
+++ internal/simd/distance_avx512.s
@@ -2,6 +2,8 @@
 
 #include "textflag.h"
 
+AVX512-optimized distance functions
+
 // ----------------------------------------------------------------------------
 // func l2SquaredAVX512Kernel(a, b unsafe.Pointer, n int) float32
 //

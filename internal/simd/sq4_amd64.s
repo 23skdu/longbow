c35db8e40e86b2b5921378aeb117b529506952f2
23skdu <23skdu@users.noreply.github.com>
2026-04-29 15:24:50 -0700

    Fix: Critical bugs in AVX2/AVX512 SQ4 kernels and stabilization of DotInt4 tests. Fixed pointer arithmetic and nibble unpacking in assembly. Fell back broken ARM64 stub to generic.

--- internal/simd/sq4_amd64.s
+++ internal/simd/sq4_amd64.s
@@ -9,40 +9,41 @@ TEXT ·dotInt4AVX512Kernel(SB), NOSPLIT, $0-28
     MOVQ    n+16(FP), BX
 
     VXORPS  Z0, Z0, Z0          // sum accumulator
-    CMPQ    BX, $32
+    VMOVDQU64 mask_low<>(SB), Z10
+    VMOVDQU64 one_word<>(SB), Z11
+
+    CMPQ    BX, $64
     JL      tail
 
 loop:
-    // Load 32 bytes (64 int4 values)
-    VMOVDQU32 (SI), Z1
-    VMOVDQU32 (DI), Z2
-
-    // Load mask into Z10
-    VMOVDQU32 mask_low<>(SB), Z10
+    // Load 64 bytes (128 int4 values)
+    VMOVDQU64 (SI), Z1
+    VMOVDQU64 (DI), Z2
 
     // Unpack low nibbles
     VPANDD   Z10, Z1, Z3
     VPANDD   Z10, Z2, Z4
+    VPMADDUBSW Z3, Z4, Z7
     
     // Unpack high nibbles
-    VPSRLD   $4, Z1, Z5
+    VPSRLW   $4, Z1, Z5
     VPANDD   Z10, Z5, Z5
-    VPSRLD   $4, Z2, Z6
+    VPSRLW   $4, Z2, Z6
     VPANDD   Z10, Z6, Z6
+    VPMADDUBSW Z5, Z6, Z8
 
-    // Convert to float and multiply-accumulate
-    VCVTDQ2PS Z3, Z3
-    VCVTDQ2PS Z4, Z4
-    VFMADD231PS Z3, Z4, Z0
-
-    VCVTDQ2PS Z5, Z5
-    VCVTDQ2PS Z6, Z6
-    VFMADD231PS Z5, Z6, Z0
+    // Sum partials
+    VPADDW   Z7, Z8, Z7
+    VPMADDWD Z11, Z7, Z7       // Z7 = 16 int32
+    
+    // Convert to float and accumulate
+    VCVTDQ2PS Z7, Z7
+    VADDPS  Z7, Z0, Z0
 
     ADDQ    $64, SI
     ADDQ    $64, DI
-    SUBQ    $32, BX
-    CMPQ    BX, $32
+    SUBQ    $64, BX
+    CMPQ    BX, $64
     JGE     loop
 
 tail:
@@ -51,9 +52,9 @@ tail:
     VADDPS  Y1, Y0, Y0
     VEXTRACTF128 $1, Y0, X1
     VADDPS  X1, X0, X0
-    VMOVHLPS X0, X1, X1
-    VADDPS  X1, X0, X0
     VMOVSHDUP X0, X1
+    VADDPS  X1, X0, X0
+    VMOVHLPS X0, X1, X1
     VADDSS  X1, X0, X0
     
     VMOVSS  X0, ret+24(FP)
@@ -67,49 +68,50 @@ TEXT ·dotInt4AVX2Kernel(SB), NOSPLIT, $0-28
     MOVQ    n+16(FP), BX
 
     VXORPS  Y0, Y0, Y0          // sum accumulator
-    CMPQ    BX, $16
+    VMOVDQU mask_low<>(SB), Y10
+    VMOVDQU one_word<>(SB), Y11
+
+    CMPQ    BX, $32
     JL      tail_avx2
 
 loop_avx2:
-    // Load 16 bytes (32 int4 values)
+    // Load 32 bytes (64 int4 values)
     VMOVDQU (SI), Y1
     VMOVDQU (DI), Y2
-
-    // Load mask into Y10
-    VMOVDQU mask_low<>(SB), Y10
 
     // Unpack low nibbles
     VPAND    Y10, Y1, Y3
     VPAND    Y10, Y2, Y4
+    VPMADDUBSW Y3, Y4, Y7
     
     // Unpack high nibbles
-    VPSRLD   $4, Y1, Y5
+    VPSRLW   $4, Y1, Y5
     VPAND    Y10, Y5, Y5
-    VPSRLD   $4, Y2, Y6
+    VPSRLW   $4, Y2, Y6
     VPAND    Y10, Y6, Y6
+    VPMADDUBSW Y5, Y6, Y8
 
-    // Convert to float and multiply-accumulate
-    VCVTDQ2PS Y3, Y3
-    VCVTDQ2PS Y4, Y4
-    VFMADD231PS Y3, Y4, Y0
-
-    VCVTDQ2PS Y5, Y5
-    VCVTDQ2PS Y6, Y6
-    VFMADD231PS Y5, Y6, Y0
+    // Sum partials
+    VPADDW   Y7, Y8, Y7
+    VPMADDWD Y11, Y7, Y7       // Y7 = 8 int32
+    
+    // Convert to float and accumulate
+    VCVTDQ2PS Y7, Y7
+    VADDPS  Y7, Y0, Y0
 
     ADDQ    $32, SI
     ADDQ    $32, DI
-    SUBQ    $16, BX
-    CMPQ    BX, $16
+    SUBQ    $32, BX
+    CMPQ    BX, $32
     JGE     loop_avx2
 
 tail_avx2:
     // Horizontal reduction
     VEXTRACTF128 $1, Y0, X1
     VADDPS  X1, X0, X0
-    VMOVHLPS X0, X1, X1
-    VADDPS  X1, X0, X0
     VMOVSHDUP X0, X1
+    VADDPS  X1, X0, X0
+    VMOVHLPS X0, X1, X1
     VADDSS  X1, X0, X0
     
     VMOVSS  X0, ret+24(FP)
@@ -125,3 +127,13 @@ DATA mask_low<>+32(SB)/8, $0x0F0F0F0F0F0F0F0F
 DATA mask_low<>+40(SB)/8, $0x0F0F0F0F0F0F0F0F
 DATA mask_low<>+48(SB)/8, $0x0F0F0F0F0F0F0F0F
 DATA mask_low<>+56(SB)/8, $0x0F0F0F0F0F0F0F0F
+
+GLOBL one_word<>(SB), (RODATA|NOPTR), $64
+DATA one_word<>+0(SB)/8, $0x0001000100010001
+DATA one_word<>+8(SB)/8, $0x0001000100010001
+DATA one_word<>+16(SB)/8, $0x0001000100010001
+DATA one_word<>+24(SB)/8, $0x0001000100010001
+DATA one_word<>+32(SB)/8, $0x0001000100010001
+DATA one_word<>+40(SB)/8, $0x0001000100010001
+DATA one_word<>+48(SB)/8, $0x0001000100010001
+DATA one_word<>+56(SB)/8, $0x0001000100010001

621cbc503dc3b7fb63b1ba6be17c7d162aa234ac
23skdu <23skdu@users.noreply.github.com>
2026-05-16 08:04:44 -0700

    release: v0.2.1-rc3

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// Version is the current version of Longbow
-	Version = "0.2.1-rc2"
+	Version = "0.2.1-rc3"
 	// Commit is the git commit hash
 	Commit = "none"
 	// BuildDate is the date when the binary was built
